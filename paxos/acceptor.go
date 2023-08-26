74c2db1f649e296d8a81a8616398a358c1a599f3
i <qxlx@ideMacBook-Pro.local>
2023-08-26 11:17:54 +0800

    update paxos

--- paxos/acceptor.go
+++ paxos/acceptor.go
@@ -1,6 +1,11 @@
 package consensus
 
-import "net"
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/rpc"
+)
 
 type Acceptor struct {
 	lis net.Listener
@@ -15,4 +20,79 @@ type Acceptor struct {
 
 	//学习者id列表
 	learners []int
+}
+
+func newAcceptor(id int, learners []int) *Acceptor {
+	acceptor := &Acceptor{
+		id:       id, //接受者的id
+		learners: learners,
+	}
+	acceptor.startServer()
+	return acceptor
+}
+
+//准备阶段
+func (a *Acceptor) Prepare(args *MsgArgs, reply *MsgReply) error {
+	if args.Number > a.minProposal {
+		a.minProposal = args.Number
+		reply.Number = a.acceptedNumber
+		reply.Value = a.acceptedValue
+		reply.Ok = true
+	} else {
+		reply.Ok = false
+	}
+	return nil
+}
+
+//接受阶段
+func (a *Acceptor) Accept(args *MsgArgs, reply *MsgReply) error {
+	if args.Number >= a.minProposal {
+		a.minProposal = args.Number
+		a.acceptedNumber = args.Number
+		a.acceptedValue = args.Value
+		reply.Ok = true
+
+		//后台执行将结果转发给学习者
+		for _, lid := range a.learners {
+			go func(learner int) {
+				addr := fmt.Sprintf("127.0.0.1:%d", learner)
+				args.From = a.id
+				args.To = learner
+				resp := new(MsgReply)
+				ok := call(addr, "Learner.Learn", args, resp)
+				if !ok {
+					return
+				}
+			}(lid)
+		}
+	} else {
+		reply.Ok = false
+	}
+	return nil
+}
+
+func (a *Acceptor) startServer() {
+	server := rpc.NewServer()
+	server.Register(a)
+	addr := fmt.Sprintf(":%d", a.id)
+	l, e := net.Listen("tcp", addr)
+	if e != nil {
+		log.Fatal("listetn is error", e)
+	}
+	a.lis = l
+
+	go func() {
+		for {
+			accept, err := a.lis.Accept()
+			if err != nil {
+				continue
+			}
+			go server.ServeConn(accept)
+		}
+	}()
+}
+
+//释放连接
+func (a *Acceptor) closeConnect() {
+	a.lis.Close()
 }
